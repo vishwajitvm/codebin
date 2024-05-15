b92947c4b939910557e00595b657d4243fce6f1d
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-15 23:42:34 +0530

    404 login : note alyway put 404 route in the end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'login' , component: LoginComponent } ,
     { path: 'signup' , component: SignupComponent },
     { path: '' , redirectTo: '/login', pathMatch:"full" }, //if we go to empty home page than it will redirect us to login page
-    
+    { path: '**' , component: NotFoundComponent }, //redirecting 404 page
+
 ];
