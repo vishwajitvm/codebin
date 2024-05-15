616bbd06c1b14b9695a83014941622c23c7b7657
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-15 23:38:15 +0530

    Routing related changes - learn how to use routing in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login' , component: LoginComponent } ,
+    { path: 'signup' , component: SignupComponent },
+    { path: '' , redirectTo: '/login', pathMatch:"full" }, //if we go to empty home page than it will redirect us to login page
+    
+];
