4729092081fd188cf5ebe21f2de886ee352b7747
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-16 16:45:54 +0530

    changes in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AboutComponent } from './components/about/about.component';
 import { CreateBinComponent } from './components/create-bin/create-bin.component';
 import { authGuard } from './guard/auth.guard';
+import { HomeComponent } from './components/home/home.component';
+import { ViewSnippetComponent } from './components/view-snippet/view-snippet.component';
 
 export const routes: Routes = [
-    { path: 'login' , component: LoginComponent } ,
-    { path: 'signup' , component: SignupComponent },
-    { path: 'about' , loadComponent: () => import('./components/about/about.component').then(mod => mod.AboutComponent) } ,
-    { path: 'create' , loadComponent: () => import('./components/create-bin/create-bin.component').then(mod => mod.CreateBinComponent) ,canActivate:[authGuard] } ,
-    { path: '' , redirectTo: '/login', pathMatch:"full" }, //if we go to empty home page than it will redirect us to login page
-    { path: '**' , component: NotFoundComponent }, //redirecting 404 page
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'about', loadComponent: () => import('./components/about/about.component').then(mod => mod.AboutComponent) },
+    { path: 'create', loadComponent: () => import('./components/create-bin/create-bin.component').then(mod => mod.CreateBinComponent), canActivate: [authGuard] },
+    // { path: '' , redirectTo: '/login', pathMatch:"full" }, //if we go to empty home page than it will redirect us to login page
+    { path: '', component: HomeComponent },
+    { path: '/snippet/:id', component: ViewSnippetComponent },
+    { path: '**', component: NotFoundComponent }, //redirecting 404 page
 
 ];
